cfe5de676bc142d83c38c7c3b1f7844ab56cb8a9
viniciussm07 <vini.soares.martins@gmail.com>
2025-11-18 01:07:57 -0300

    Feat: corrigindo atividade coverage

--- atividade-coverage/funcional.py
+++ atividade-coverage/funcional.py
@@ -25,24 +25,29 @@ class TestPlacar(unittest.TestCase):
         self.assertEqual(str(self.p1), expected)
     
     def test_uma_linha(self):
-        # verifica todas as posições vazias: 0..8 retornam '(n) ' e 9 retorna '(10)'
         expected_empty = ['(1) ', '(2) ', '(3) ', '(4) ', '(5) ', '(6) ', '(7) ', '(8) ', '(9) ', '(10)']
         for i in range(10):
             self.assertEqual(self.p1.uma_linha(i), expected_empty[i])
 
-        # agora marca todas as posições como ocupadas e define valores distintos
         for i in range(10):
             self.p1.placar[i] = i * 10 + 1
             self.p1.taken[i] = True
 
-        # quando ocupada, deve retornar o número formatado centralizado com largura 4
         expected_taken = [('{:^4d}'.format(self.p1.placar[i])) for i in range(10)]
         for i in range(10):
             self.assertEqual(self.p1.uma_linha(i), expected_taken[i])
     
     def test_add_posicao_invalida(self):
         with self.assertRaises(IndexError) as cm:
-            self.p1.add(12 , [1, 2, 3, 4, 5])
+            self.p1.add(0, [1, 2, 3, 4, 5])
+        self.assertEqual(str(cm.exception), "Valor da posição no placar é ilegal")
+        
+        with self.assertRaises(IndexError) as cm:
+            self.p1.add(11, [1, 2, 3, 4, 5])
+        self.assertEqual(str(cm.exception), "Valor da posição no placar é ilegal")
+        
+        with self.assertRaises(IndexError) as cm:
+            self.p1.add(12, [1, 2, 3, 4, 5])
         self.assertEqual(str(cm.exception), "Valor da posição no placar é ilegal")
 
     def test_add_posicao_ocupada(self):
@@ -52,8 +57,16 @@ class TestPlacar(unittest.TestCase):
         self.assertEqual(str(cm.exception), "Posição ocupada no placar")
     
     def test_add_posicoes_1_a_6_validas(self):
+        dados_variados = [
+            [1, 1, 2, 3, 4],
+            [2, 2, 3, 4, 5],
+            [3, 3, 4, 5, 6],
+            [4, 4, 1, 2, 6],
+            [5, 5, 1, 2, 3],
+            [6, 6, 1, 2, 3]
+        ]
         for posicao in range(1, 7):
-            self.p1.add(posicao, [1, 2, 3, 4, 5])
+            self.p1.add(posicao, dados_variados[posicao - 1])
             self.assertTrue(self.p1.taken[posicao - 1])
             
     def test_add_posicao_7_satisfeita(self):
@@ -108,22 +121,24 @@ class TestPlacar(unittest.TestCase):
         self.assertEqual(self.p1.getScore(), 0)
 
     def test_getScore_com_parametro(self):
-        dados = [1, 1, 1, 1, 1] 
-        self.p1.add(10, dados)
+        self.p1.add(1, [1, 1, 1, 1, 1])
+        self.p1.add(2, [2, 2, 2, 2, 2])
+        self.p1.add(3, [3, 3, 3, 3, 3])
+        self.p1.add(10, [1, 1, 1, 1, 1])
+        
+        self.assertEqual(self.p1.getScore(0), 5)
+        self.assertEqual(self.p1.getScore(1), 10)
+        self.assertEqual(self.p1.getScore(2), 15)
         self.assertEqual(self.p1.getScore(9), 40)
 
     def test_getScore_index_nao_ocupado(self):
-        # quando pedimos um índice específico que ainda não foi ocupado,
-        # getScore deve retornar o valor armazenado (inicialmente 0)
         self.assertEqual(self.p1.getScore(0), 0)
+        self.assertEqual(self.p1.getScore(5), 0)
+        self.assertEqual(self.p1.getScore(9), 0)
 
     def test_getScore_soma_varias_posicoes(self):
-        # adiciona algumas posições e confere se a soma total é correta
-        # pos 1: cinco uns -> 5
         self.p1.add(1, [1, 1, 1, 1, 1])
-        # pos 2: cinco dois -> 10
         self.p1.add(2, [2, 2, 2, 2, 2])
-        # pos 10 (index 9): quina -> 40
         self.p1.add(10, [1, 1, 1, 1, 1])
         self.assertEqual(self.p1.getScore(), 5 + 10 + 40)
     
@@ -138,8 +153,17 @@ class TestPlacar(unittest.TestCase):
     def test_conta_0_ocorrencias(self):
         self.assertEqual(self.p1.conta(3, [1, 2, 4, 5, 6]), 0)
 
-    def test_conta_qualquer_ocorrencia(self):
+    def test_conta_1_ocorrencia(self):
+        self.assertEqual(self.p1.conta(3, [1, 2, 3, 4, 5]), 1)
+
+    def test_conta_2_ocorrencias(self):
         self.assertEqual(self.p1.conta(1, [1, 1, 3, 3, 3]), 2)
+
+    def test_conta_3_ocorrencias(self):
+        self.assertEqual(self.p1.conta(2, [2, 2, 2, 3, 4]), 3)
+
+    def test_conta_4_ocorrencias(self):
+        self.assertEqual(self.p1.conta(4, [4, 4, 4, 4, 5]), 4)
 
     def test_conta_5_ocorrencias(self):
         self.assertEqual(self.p1.conta(1, [1, 1, 1, 1, 1]), 5)
