d0d54e36e67f54acdb7db65b1966c9845bd1aa82
viniciussm07 <vini.soares.martins@gmail.com>
2025-12-01 22:39:27 -0300

    Feat: add mutations tests

--- atividade-pyproteum/mutation.py
+++ atividade-pyproteum/mutation.py
@@ -12,3 +12,21 @@ def test_posicao_9_sem_quadra():
     dados = [1, 2, 3, 4, 5]   
     p.add(9, dados)
     assert p.placar[8] == 0
+
+
+# ==EQUIVALENTE MUTANTE 1212==
+def test_posicao_7_full():
+    from placar import Placar
+    p = Placar()
+    dados = [2,2,2,3,3]  
+    p.add(7, dados)
+    assert p.placar[6] == 15  
+
+
+# ==1208==
+def test_posicao_1_com_dados():
+    from placar import Placar
+    p = Placar()
+    dados = [1,1,2,3,4]  
+    p.add(1, dados)
+    assert p.placar[0] == 1
