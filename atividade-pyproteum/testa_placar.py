528632c7f85702daf9ecbf4395d0c0992d8034bd
viniciussm07 <vini.soares.martins@gmail.com>
2025-12-01 22:01:27 -0300

    Feat: add testa_placar

---
+++ atividade-pyproteum/testa_placar.py
@@ -0,0 +1,20 @@
+import os
+
+# Limpeza rápida para garantir que não dê erro de "sessão já existe" ao rodar de novo
+# if os.path.exists("placar_session.xsml"):
+#     os.remove("placar_session.xsml")
+# if os.path.exists("placar_session.db"):
+#     os.remove("placar_session.db")
+
+os.system("python3 -m pyproteum testnew --D . --S placar.py placar_session")
+
+# Adiciona funcional.py e mcdc.py na mesma linha conforme seu comando
+os.system("python3 -m pyproteum tcase --add --S funcional.py mcdc.py placar_session")
+
+os.system("python3 -m pyproteum mutagen --create --all 100 0 placar_session")
+
+os.system("python3 -m pyproteum exemuta --exec placar_session")
+
+os.system("python3 -m pyproteum mutaview --list placar_session")
+# Descomente a linha abaixo para abrir a janela visual (isso pausa o script)
+# os.system("python3 -m pyproteum mutaview --gui placar_session")
