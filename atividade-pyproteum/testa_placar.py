929c33b6b5293a60c4d333ae75c3cd05cea141d9
viniciussm07 <vini.soares.martins@gmail.com>
2025-12-07 21:13:19 -0300

    Chore: removendo comentários e atualizando section db

--- atividade-pyproteum/testa_placar.py
+++ atividade-pyproteum/testa_placar.py
@@ -4,12 +4,6 @@
 
 import os
 
-# Limpeza rápida para garantir que não dê erro de "sessão já existe" ao rodar de novo
-# if os.path.exists("placar_session.xsml"):
-#     os.remove("placar_session.xsml")
-# if os.path.exists("placar_session.db"):
-#     os.remove("placar_session.db")
-
 os.system("python3 -m pyproteum testnew --D . --S placar.py placar_session")
 
 os.system("python3 -m pyproteum tcase --add --S funcional.py placar_session")
@@ -18,8 +12,6 @@ os.system("python3 -m pyproteum tcase --add --S mutation.py placar_session")
 
 os.system("python3 -m pyproteum mutagen --create --all 100 0 placar_session")
 
-# os.system("python3 -m pyproteum exemuta --equiv --x 648 placar_session")
-# os.system("python3 -m pyproteum exemuta --equiv --x 635 placar_session")
 os.system("python3 -m pyproteum exemuta --equiv --x 0079 placar_session")
 os.system("python3 -m pyproteum exemuta --equiv --x 0080 placar_session")
 os.system("python3 -m pyproteum exemuta --equiv --x 0159 placar_session")
